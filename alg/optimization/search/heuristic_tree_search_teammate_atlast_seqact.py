eb23ebfb005c701db463aea3a021fc653393d58b
penggao00 <penggao00@hotmail.com>
2025-05-21 01:29:43 +0200

    at last search; overcooked2.py wrapped info with action

---
+++ alg/optimization/search/heuristic_tree_search_teammate_atlast_seqact.py
@@ -0,0 +1,11 @@
+from alg.optimization.algs.heuristic_ts_alg_teammate_atlast_seqact import HeuristicTSAlgorithmTM
+from alg.optimization.search.tree_search import TreeSearch
+
+
+class HeuristicTreeSearch(TreeSearch):
+
+    def __init__(self, env, bb_model, obj, params):
+        alg = HeuristicTSAlgorithmTM(env, bb_model, obj, params)
+        super(HeuristicTreeSearch, self).__init__(env, bb_model, obj, params, alg)
+
+
